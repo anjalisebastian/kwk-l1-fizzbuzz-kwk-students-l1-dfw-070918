6167ca8eee52a7b1a114038ada2f22c37bfbaa48
anjalisebastian <anjalisebastian@icloud.com>
2018-07-10 18:25:24 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
 def fizzbuzz(n)
-if n%3==0 && n%5==0 
-  puts "FizzBuzz"
-elsif n%3==0 
-  puts "Fizz" 
-else n%5==0 
-  puts "Buzz"
+  if n%3==0 && n%5==0 
+    puts "FizzBuzz"
+  elsif n%3==0 
+    puts "Fizz" 
+  else n%5==0 
+    puts "Buzz"
 end
